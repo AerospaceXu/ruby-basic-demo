e90e31b16f9fdaa6834b26dd2e7e6628715becaf
AerospaceXu <aerospacexu@gmail.com>
2020-12-27 20:30:23 +0800

    feat: repeat define class will merge them.

---
+++ about-class-2.rb
@@ -0,0 +1,20 @@
+class XXX
+  def x
+    'x'
+  end
+end
+
+class XXX
+  def y
+    'y'
+  end
+end
+
+obj = XXX.new
+
+puts obj.x
+puts obj.y
+
+# these two classes are been "merged" automatically!!!
+#
+# 这两个 class 被 Ruby 自动"合并"了！！！
--- about-class.rb
+++ about-class.rb
@@ -6,6 +6,8 @@ end
 
 # you can do anything in class without function
 # they will work!!!
+# next you should read the code in about-class-2
 #
 # 你可以在 class 里面运行任何的代码，不需要方法，不需要函数
 # 他们都可以直接执行
+# 接下来你应该阅读 about-class-2 中的代码
